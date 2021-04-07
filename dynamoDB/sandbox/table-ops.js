9d09cff77a5e2dec6715355c3af0dd83398aa004
meohyun2 <eogus6242@gmail.com>
2021-04-08 01:45:12 +0900

    feat: table level operation added

--- dynamoDB/sandbox/table-ops.js
+++ dynamoDB/sandbox/table-ops.js
@@ -24,3 +24,58 @@ dynamodb.describeTable({
     console.log(JSON.stringify(data, null, 2));
   }
 })
+
+dynamodb.createTable({
+  TableName: "dummyComments",
+  AttributeDefinitions: [
+    {
+      AttributeName: "user_id",
+      AttributeType: "S",
+    },
+    {
+      AttributeName: "timestamp",
+      AttributeType: "N",
+    },
+  ],
+  KeySchema: [
+    {
+      AttributeName: "user_id",
+      KeyType: "HASH",
+    },
+    {
+      AttributeName: "timestamp",
+      KeyType: "RANGE"
+    },
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 1,
+    WriteCapacityUnits: 1
+  }
+},(err, data)=> {
+  if(err){
+    console.log(err);
+  }else{
+    console.log(data);
+  }
+})
+
+dynamodb.updateTable({
+  TableName: "dummyComments",
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 2,
+    WriteCapacityUnits: 1
+  },
+},(err, data)=>{
+  if(err){
+    console.log(err);
+  }else{
+    console.log(data);
+  }
+})
+
+dynamodb.deleteTable({
+  TableName: "dummyComments"
+}, (err,data)=> {
+  if(err)console.log(err);
+  else console.log(data);
+})
